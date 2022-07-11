20834c0922cec354145086a4eb3b4f32b9f7bacf
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-07-11 22:20:50 +0000

    first iteration of static HTML of the audio player

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
 import React from "react";
-
+import {
+  FaPlayCircle,
+  FaPauseCircle,
+  FaFastForward,
+  FaFastBackward,
+} from "react-icons/fa";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="container-fluid">
+      <div className="row bg-dark">
+        <div className="col-12-md p-0">
+          <ul className="list-group list-group-flush p-2">
+            <li className="list-group-item bg-dark text-white">An item</li>
+            <li className="list-group-item bg-dark text-white">
+              A second item
+            </li>
+            <li className="list-group-item bg-dark text-white">A third item</li>
+            <li className="list-group-item bg-dark text-white">
+              A fourth item
+            </li>
+            <li className="list-group-item bg-dark text-white">
+              And a fifth one
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="fixed-bottom">
+        <div className="row bg-secondary p-3 justify-content-center">
+          <div id="backward" className="col-4 text-end">
+            <FaFastBackward size='50'/>
+          </div>
+          <div className="col-2 text-center">
+            <FaPlayCircle size='50'/>
+          </div>
+          <div className="col-4 text-start">
+            <FaFastForward size='50'/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
