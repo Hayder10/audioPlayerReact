9e97fc96de4e7b16ee2d0bb4e190299610b8abf0
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-07-14 05:04:44 +0000

    implemented api song title fetching and rendering

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   FaPlayCircle,
   FaPauseCircle,
@@ -6,44 +6,51 @@ import {
   FaFastBackward,
 } from "react-icons/fa";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  const [songList, setSongList] = useState([]);
+
+  const url = 'http://assets.breatheco.de/apis/sound/songs';
+  
+  useEffect(() => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => setSongList(data))
+    .catch(error => console.log(error))
+  }, []) 
+  var contentSong = songList.map((element) => 
+  <li className="list-group-item bg-dark text-white" onmouseover="" key={ element.id }>
+{ element.name }
+  </li>
+  )
+
+
+
   return (
     <div className="container-fluid">
       <div className="row bg-dark">
         <div className="col-12-md p-0">
           <ul className="list-group list-group-flush p-2">
-            <li className="list-group-item bg-dark text-white">An item</li>
-            <li className="list-group-item bg-dark text-white">
-              A second item
-            </li>
-            <li className="list-group-item bg-dark text-white">A third item</li>
-            <li className="list-group-item bg-dark text-white">
-              A fourth item
-            </li>
-            <li className="list-group-item bg-dark text-white">
-              And a fifth one
-            </li>
+            { contentSong }
           </ul>
         </div>
       </div>
       <div className="fixed-bottom">
         <div className="row bg-secondary p-3 justify-content-center">
           <div id="backward" className="col-4 text-end">
-            <FaFastBackward size='50'/>
+            <FaFastBackward size='50' />
           </div>
           <div className="col-2 text-center">
-            <FaPlayCircle size='50'/>
+            <FaPlayCircle size='50' />
           </div>
           <div className="col-4 text-start">
-            <FaFastForward size='50'/>
+            <FaFastForward size='50' />
           </div>
         </div>
       </div>
     </div>
   );
-};
+  }
 
 export default Home;
